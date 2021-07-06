b235d9d7831ae2dab7d6b3161c9a90064580a741
Liz Wilson <lizriderwilson@gmail.com>
2021-07-06 12:35:21 -0400

    commit tree 7f607cb213c629c3471d8a8e78bfa15118086535

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array, callback) {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i]));
+  }
+  return result;
+}
+
+function reduce(array, callback, startingPoint) {
+  let total;
+  if (startingPoint) {
+    total = startingPoint;
+    for (let i = 0; i < array.length; i++) {
+      total = callback(array[i], total);
+    }
+  } else {
+    total = array[0];
+    for (let i = 1; i < array.length; i++) {
+      total = callback(array[i], total);
+    }
+  }
+  return total;
+}
